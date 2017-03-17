f5cae4435f59fad22009bc3f49f8107c75ebb4b6
dai <daipresents@gmail.com>
2017-03-17 16:02:03 +0900

    Move find method to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,3 +22,18 @@ RSpec.configure { |c|
     @driver.quit
   }
 }
+
+module D
+  def self.find(*args)
+    what = args[0]
+    Appium::Logger.debug "D.find: #{what}"
+
+    if what.start_with?('//')
+      # xpath
+      RSpec.driver.find_element(:xpath, what)
+    else
+      # id
+      RSpec.driver.find_element(:id, what)
+    end
+  end
+end
